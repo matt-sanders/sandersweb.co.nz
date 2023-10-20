def0914fb469333bc2e6c36610d39083904fd3b6
Matt Sanders <matt@sandersweb.co.nz>
2023-10-20 14:51:17 +1300

    Centers grid lines

--- src/components/GridLines/GridLines.tsx
+++ src/components/GridLines/GridLines.tsx
@@ -6,15 +6,18 @@ export function GridLines() {
     <div className="absolute inset-0">
       <Container className="h-full">
         <div className="relative h-full">
-          <div className="grid grid-cols-4 md:grid-cols-12 h-full">
+          <div className="grid grid-cols-4 md:grid-cols-12 h-full gap-24px border-dashed border-l border-r border-dark-900/20">
             {[...Array(12)].map((_, idx) => (
               <div
                 key={idx}
-                className={clsx("border-r border-dashed border-dark-900/20", {
-                  "border-l": idx === 0,
+                className={clsx("relative", {
                   "hidden md:block": idx >= 4,
                 })}
-              />
+              >
+                {idx > 0 && (
+                  <div className="absolute top-0 bottom-0 right-full border-l transform -translate-x-12px border-dashed border-dark-900/20" />
+                )}
+              </div>
             ))}
           </div>
         </div>
