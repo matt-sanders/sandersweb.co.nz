12a2e699d91f64035e5de385d937cf73a6cafff0
Matt Sanders <matt@sandersweb.co.nz>
2023-10-27 16:14:05 +1300

    Adds logo to project card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,18 +1,29 @@
 import { LinkButton } from '@/components/LinkButton/LinkButton'
+import { ProjectIcons } from '@/components/ProjectIcons/ProjectIcons'
 import { Shadow } from '@/components/Shadow/Shadow'
-import { ProjectSummary } from '@/domain/projects'
+import { ProjectRecord } from '@/domain/projects'
 import { routes } from '@/routes'
+import clsx from 'clsx'
 
 interface ProjectCardProps {
-  project: ProjectSummary
+  project: ProjectRecord
 }
 export function ProjectCard({ project }: ProjectCardProps) {
+  const Icon =
+    (project.icon && ProjectIcons[project.icon]) || ProjectIcons.Default
   return (
     <Shadow className="w-full">
       <div className="space-y-12px px-12px pt-12px pb-18px">
-        <div className="h-72px bg-[#eaeaea]"></div>
-        <h3 className="font-semibold">{project.org}</h3>
-        <p className="text-heading-500">{project.name}</p>
+        <div
+          className={clsx(
+            'p-24px flex items-center justify-center rounded',
+            project.bgColor || 'bg-dark-900/10',
+          )}
+        >
+          <Icon className="w-72px" />
+        </div>
+        <h3 className="text-heading-500">{project.name}</h3>
+        <p className="font-semibold">{project.company}</p>
         <p>{project.summary}</p>
         <LinkButton href={routes.projects.single(project)}>
           Read more
