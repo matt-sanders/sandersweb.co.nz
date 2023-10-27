e4590f30a288e5bf185cef594a2fff9ee76efee6
Matt Sanders <matt@sandersweb.co.nz>
2023-10-27 16:13:50 +1300

    Adds project page

--- src/layouts/ProjectLayout/ProjectLayout.tsx
+++ src/layouts/ProjectLayout/ProjectLayout.tsx
@@ -1,64 +1,107 @@
 import { Container } from '@/components/Container/Container'
 import { ProjectIcons } from '@/components/ProjectIcons/ProjectIcons'
 import { Shadow } from '@/components/Shadow/Shadow'
+import { ProjectRecord } from '@/domain/projects'
 import clsx from 'clsx'
 import { useEffect, useState } from 'react'
 
 interface ProjectLayoutProps {
-  title: string
-  bgColor?: string
   children: React.ReactNode
-  icon?: string
-  tech?: string[]
-  company?: string
+  project: ProjectRecord
 }
-export function ProjectLayout({
-  title,
-  bgColor,
-  children,
-  icon,
-  tech,
-  company,
-}: ProjectLayoutProps) {
+export function ProjectLayout({ children, project }: ProjectLayoutProps) {
   const [pageReady, setPageReady] = useState(false)
   useEffect(() => {
     setPageReady(true)
   }, [])
 
+  const { name, tech, company, icon, bgColor } = project
+
   const Icon = (icon && ProjectIcons[icon]) || ProjectIcons.Default
   return (
     <main>
       <Container>
-        <Shadow className="my-48px w-full">
+        <Shadow
+          className={clsx(
+            'my-48px w-full',
+            'transition-all duration-500 ease-out',
+            {
+              '-translate-y-1/4 opacity-0': !pageReady,
+              ' opacity-100': pageReady,
+            },
+          )}
+          popDelayMs={1000}
+        >
           <div
-            className={clsx(
-              'p-24px gap-24px md:gap-48px grid grid-cols-12 overflow-hidden',
-              //'overflow-hidden transition-all delay-500 duration-500 ease-in',
-              {
-                // 'max-h-0 opacity-0': !pageReady,
-                // 'max-h-[1000px] opacity-100': pageReady,
-              },
-            )}
+            className={clsx('p-24px gap-24px md:gap-48px grid grid-cols-12')}
           >
             <div
               className={clsx(
                 'px-24px py-48px col-span-12 flex min-h-[100px] items-center justify-center rounded md:col-span-4',
                 bgColor || 'bg-dark-900/10',
+                'transition-all delay-[400ms] duration-500 ease-out',
+                {
+                  'translate-x-1/4 opacity-0': !pageReady,
+                  ' opacity-100': pageReady,
+                },
               )}
             >
               <Icon className="w-72px" />
             </div>
 
             <div className="col-span-12 md:col-span-8">
-              <h1 className="text-heading-700">{title}</h1>
+              <div className="text-heading-700 pb-16px mb-16px md:pb-24px overflow-hidden border-b-2">
+                <h1
+                  className={clsx(
+                    'text-heading-700',
+                    'transition-all delay-[600ms] duration-500 ease-out',
+                    {
+                      'translate-y-1/4 opacity-0': !pageReady,
+                      ' opacity-100': pageReady,
+                    },
+                  )}
+                >
+                  {name}
+                </h1>
+              </div>
               {!!company && (
-                <p className="text-heading-500 mt-16px">{company}</p>
+                <div
+                  className={clsx(
+                    'transition delay-500 duration-500 ease-out',
+                    {
+                      'opacity-0': !pageReady,
+                      ' opacity-100': pageReady,
+                    },
+                  )}
+                >
+                  <p
+                    className={clsx(
+                      'text-heading-500',
+                      'transition-all delay-[800ms] duration-500 ease-out',
+                      {
+                        '-translate-y-1/4 opacity-0': !pageReady,
+                        ' opacity-100': pageReady,
+                      },
+                    )}
+                  >
+                    {company}
+                  </p>
+                </div>
               )}
               {tech && (
-                <ul className="gap-12px mt-16px flex flex-wrap">
-                  {tech.map((techItem) => (
+                <ul
+                  className={clsx(
+                    'gap-12px mt-16px flex flex-wrap',
+                    'transition delay-[800ms] duration-500 ease-out',
+                    {
+                      'opacity-0': !pageReady,
+                      'opacity-100': pageReady,
+                    },
+                  )}
+                >
+                  {tech.map((techItem, idx) => (
                     <li key={techItem}>
-                      <Shadow>
+                      <Shadow popDelayMs={1000 + idx * 80}>
                         <div className="px-6px">{techItem}</div>
                       </Shadow>
                     </li>
---
+++ src/pages/projects/[projectSlug]/index.tsx
@@ -0,0 +1,46 @@
+import { ProjectRecord } from '@/domain/projects'
+import { getAllProjects, getProjectBySlug } from '@/lib/api'
+import { GetStaticProps } from 'next'
+import { MDXRemote } from 'next-mdx-remote'
+
+interface Props {
+  layout: string
+  project: ProjectRecord
+}
+
+export const getStaticPaths = async () => {
+  const projects = await getAllProjects()
+  return Promise.resolve({
+    fallback: false,
+    paths: projects.map((project) => ({
+      params: {
+        projectSlug: project.slug,
+      },
+    })),
+  })
+}
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const slug = Array.isArray(params?.projectSlug)
+    ? params?.projectSlug[0]
+    : params?.projectSlug
+  if (!slug)
+    return {
+      notFound: true,
+    }
+
+  const project = await getProjectBySlug(slug)
+  return {
+    props: {
+      layout: 'ProjectLayout',
+      project,
+    },
+  }
+}
+
+export default function ProjectPage({ project }: Props) {
+  console.log(project)
+  return (
+    <MDXRemote compiledSource={project.content} scope={{}} frontmatter={{}} />
+  )
+}
