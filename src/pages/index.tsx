09aff6c2ddfb232c5f35d6786e13a1f22d4e4643
Matt Sanders <matt@sandersweb.co.nz>
2023-11-22 06:40:08 +1300

    Animates about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import clsx from 'clsx'
 import { GetStaticProps } from 'next'
 import { useRef } from 'react'
 import { PageLayout } from '@/layouts/PageLayout/PageLayout'
-import { useScroll, useTransform, motion } from 'framer-motion'
+import { useScroll, useTransform, motion, MotionValue } from 'framer-motion'
 import { Logo } from '@/components/Logo/Logo'
 
 interface Props {
@@ -23,7 +23,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
   }
 }
 
-function HiContainer() {
+function Intro() {
   const screenOneRef = useRef(null)
   const { scrollYProgress: screenOneScrollYProgress } = useScroll({
     target: screenOneRef,
@@ -82,23 +82,101 @@ function HiContainer() {
   )
 }
 
+interface RevealCharProps {
+  char: string
+  progress: MotionValue<number>
+  offset: number
+}
+function RevealChar({ char, progress, offset }: RevealCharProps) {
+  const speed = 0.15
+  const delay = 0.1
+  const transformTimes = [delay + offset, delay + offset + speed]
+  const yPos = useTransform(progress, transformTimes, ['100%', '0%'])
+  const opacity = useTransform(progress, transformTimes, [0, 1])
+  return (
+    <motion.span className="inline-block" style={{ y: yPos, opacity }}>
+      {char === ' ' ? <>&nbsp;</> : char}
+    </motion.span>
+  )
+}
+
+interface RevealTextProps {
+  text: string
+  progress: MotionValue<number>
+}
+function RevealText({ text, progress }: RevealTextProps) {
+  const chars = text.split('')
+  // we want all the text to be visible within 70% of the scroll progress
+  const offsetMultiplier = 0.7 / chars.length
+  // assign the character offset to each one, to be used when we map this back to words
+  const charMap = chars.map((char, index) => ({
+    char,
+    offset: index * offsetMultiplier,
+  }))
+  // Each char is eventually going to be wrapped in a span with inline-block applied to it.
+  // This will result in some funky word breaks.
+  // To amend this, we need to wrap each set of characters in a span also.
+  const words = charMap.reduce<(typeof charMap)[number][][]>(
+    (accumulator, char) => {
+      if (char.char === ' ') {
+        return [...accumulator, [char], []]
+      }
+      const nextAccumulator = [...accumulator]
+      const lastWord = nextAccumulator.pop()
+      if (!lastWord) {
+        return [[char]]
+      }
+      return [...nextAccumulator, [...lastWord, char]]
+    },
+    [],
+  )
+  return (
+    <span>
+      {words.map((word, wordIdx) => (
+        <span className="inline-block" key={wordIdx}>
+          {word.map((char, charIndex) => (
+            <RevealChar
+              key={charIndex}
+              char={char.char}
+              offset={char.offset}
+              progress={progress}
+            />
+          ))}
+        </span>
+      ))}
+    </span>
+  )
+}
+
+function About() {
+  const targetRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ['start end', 'start start'],
+  })
+  return (
+    <div className="relative h-screen" ref={targetRef}>
+      <Container className="pt-24px sticky top-0">
+        <h1 className="text-shadow shadow-secondary-900 text-heading-900 mb-48px">
+          I&apos;m Matt
+        </h1>
+        <p className="text-700 max-w-2xl">
+          <RevealText
+            text="I make cool stuff and have been doing so for the last 12 years. From APIs and integrations to spiffy front end animations, I do it all. Check out some of the things I've worked on below."
+            progress={scrollYProgress}
+          />
+        </p>
+      </Container>
+    </div>
+  )
+}
+
 export default function Home({ projects }: Props) {
   return (
     <PageLayout>
       <main>
-        <HiContainer />
-        <div className="relative h-screen">
-          <Container className="sticky top-0">
-            <h1 className="text-shadow shadow-secondary-900 text-heading-900 mb-48px">
-              I&apos;m Matt
-            </h1>
-            <p className="text-700 max-w-2xl">
-              I make cool stuff and have been doing so for the last 12 years.
-              From APIs and integrations to spiffy front end animations, I do it
-              all. Check out some of the things I&apos;ve worked on below.
-            </p>
-          </Container>
-        </div>
+        <Intro />
+        <About />
         <Container>
           <section className="my-72px" id="projects">
             <h2 className="text-heading-700 mb-24px md:mb-48px">Projects</h2>
@@ -113,41 +191,5 @@ export default function Home({ projects }: Props) {
         </Container>
       </main>
     </PageLayout>
-  )
-}
-
-interface RevealTextProps {
-  text: string
-  reveal: boolean
-  idx: number
-}
-function RevealText({ text, reveal, idx }: RevealTextProps) {
-  const offset = 100
-  const delayBase = 300
-  const delayMs = delayBase + offset * idx
-  return (
-    <span className="inline-block overflow-hidden">
-      <span
-        className={clsx('inline-block transform transition', {
-          'translate-y-full': !reveal,
-        })}
-        style={{
-          transitionDelay: `${delayMs}ms`,
-        }}
-      >
-        <span
-          aria-hidden
-          className={clsx(
-            'text-secondary-900 ease-bounce absolute left-0 top-0 transform transition delay-1000',
-            {
-              'translate-y-shadow-offset -translate-x-shadow-offset': reveal,
-            },
-          )}
-        >
-          {text}
-        </span>
-        <span className="relative">{text}</span>
-      </span>
-    </span>
   )
 }
