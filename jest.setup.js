47c6e4348ad152d2a717d80473d71f6f60b5535c
Matt Sanders <matt@sandersweb.co.nz>
2023-11-27 06:00:13 +1300

    Mocks intersection observer

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,8 @@
 // Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom'
+import { act } from '@testing-library/react'
+import { configMocks, mockIntersectionObserver } from 'jsdom-testing-mocks'
+
+configMocks({ act })
+
+mockIntersectionObserver()
